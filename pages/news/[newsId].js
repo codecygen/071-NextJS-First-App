b396d808b1f62b42bfa863244f681e448a9259b7
gingercook <vahitaras@gmail.com>
2022-04-25 21:58:30 -0400

    end of day

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,12 @@
 // http://our-domain/news/something-important
 
+import { useRouter } from "next/router";
+
 const DetailPage = () => {
+    const router = useRouter();
+
+    router.query
+
     return (
       <h1>Detail Page</h1>
     );
